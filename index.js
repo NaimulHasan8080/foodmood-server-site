7ee6a252adc61533bc71a88613a6e0083de6ddcc
unknown <naimul8080@gmail.com>
2021-10-31 13:03:21 +0600

    update delete

--- index.js
+++ index.js
@@ -88,7 +88,7 @@ async function run() {
         })
 
         // cancel an order
-        app.delete('/myorders/:id', async (req, res) => {
+        app.delete('/orders/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const result = await ordersCollection.deleteOne(query);
